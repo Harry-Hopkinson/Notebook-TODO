fa5ca039640f0d6d8f5fbd04ee6c843bca97ebf2
Harry Hopkinson <harryjhopkinson@gmail.com>
2021-12-10 07:39:33 +0000

    Remark is set up for a live feed of what you are making notes on

--- packages/renderer/src/app.tsx
+++ packages/renderer/src/app.tsx
@@ -12,7 +12,7 @@ const App: React.FC = () => {
     return (
         <div className="app">
             <Editor onChange={handleDocChange} initialDoc={doc}/>
-            <Preview />
+            <Preview doc={doc}/>
         </div>
     )
 }
