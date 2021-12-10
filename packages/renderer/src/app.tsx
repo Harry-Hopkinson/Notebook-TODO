73ac942e012bda2cb76506a2700d13b7755988b8
Harry Hopkinson <harryjhopkinson@gmail.com>
2021-12-10 07:54:27 +0000

    Fixed css code

--- packages/renderer/src/app.tsx
+++ packages/renderer/src/app.tsx
@@ -1,20 +1,21 @@
-import React, { useCallback, useState } from "react";
-import Editor from "./editor";
-import Preview from "./preview";
-import "./app.css";
+import React, { useState, useCallback } from 'react'
+import Editor from './editor'
+import Preview from './preview'
+import './app.css'
 
 const App: React.FC = () => {
-    const [doc, setDoc] = useState<string>("# Hello, World!\n");
+  const [doc, setDoc] = useState<string>('# Hello, World!\n')
 
-    const handleDocChange = useCallback(newDoc => {
-        setDoc(newDoc)
-    }, [])
-    return (
-        <div className="app">
-            <Editor onChange={handleDocChange} initialDoc={doc}/>
-            <Preview doc={doc}/>
-        </div>
-    )
+  const handleDocChange = useCallback(newDoc => {
+    setDoc(newDoc)
+  }, [])
+
+  return (
+    <div className='app'>
+      <Editor onChange={handleDocChange} initialDoc={doc} />
+      <Preview doc={doc} />
+    </div>
+  )
 }
 
-export default App;
+export default App
--- packages/renderer/src/editor.tsx
+++ packages/renderer/src/editor.tsx
@@ -1,29 +1,30 @@
-import React, { useCallback, useEffect} from "react";
-import useCodeMirror from "./use-codemirror";
-import "./editor.css";
+import React, { useCallback, useEffect } from 'react'
+import useCodeMirror from './use-codemirror'
+import './editor.css'
 
 interface Props {
-    initialDoc: string,
-    onChange: (doc: string) => void
+  initialDoc: string,
+  onChange: (doc: string) => void
 }
 
 const Editor: React.FC<Props> = (props) => {
-    const { onChange, initialDoc } = props
-    const handleChange = useCallback(
-        state => onChange(state.doc.toString()),
-        [onChange]
-    )
-    const [refContainer, editorView] = useCodeMirror<HTMLDivElement>({
-        initialDoc: initialDoc,
-        onChange: handleChange
-      })
+  const { onChange, initialDoc } = props
+  const handleChange = useCallback(
+    state => onChange(state.doc.toString()),
+    [onChange]
+  )
+  const [refContainer, editorView] = useCodeMirror<HTMLDivElement>({
+    initialDoc: initialDoc,
+    onChange: handleChange
+  })
 
-    useEffect(() => {
-        if (editorView) {
-            // Do nothing for now
-        }
-    }, [editorView])
-    return <div className="editor-wrapper" ref={refContainer}></div>
+  useEffect(() => {
+    if (editorView) {
+      // Do nothing for now
+    }
+  }, [editorView])
+
+  return <div className='editor-wrapper' ref={refContainer}></div>
 }
 
-export default Editor;
+export default Editor
